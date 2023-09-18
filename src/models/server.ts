de5cd7d0a89c2e022d24328470a1a1e1aa68060e
Gab Silv <gabsilv219@gmail.com>
2023-09-18 18:45:50 -0400

    modified port in server.ts

--- src/models/server.ts
+++ src/models/server.ts
@@ -12,7 +12,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT || '3000';
+        this.port = process.env.PORT || '3001';
         this.listen();
         this.midlewares();
         this.routes();
